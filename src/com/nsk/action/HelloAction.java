6a6b68f77f17bd2d11e547496ff501c9a5239d21
nsk-devil <nsjkabc@gmail.com>
2018-03-02 11:30:14 +0800

    Initial commit

---
+++ src/com/nsk/action/HelloAction.java
@@ -0,0 +1,19 @@
+package com.nsk.action;
+
+/**
+ * 春风再美也比不上你的笑。。。。。
+ *
+ * @author niushuaike
+ * @create 2018/3/2
+ */
+public class HelloAction {
+
+
+    /*
+    * 访问action时默认执行execute方法
+    * */
+    public String execute(){
+        return "ok";
+    }
+
+}
